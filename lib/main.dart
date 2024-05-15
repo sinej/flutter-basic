97d0b4dc15b9ea430f83be231a5c06cf77cd3931
shin <sinej1020@gmail.com>
2024-05-15 22:10:19 +0900

    feat: statelessWidget 선언 및 action 사용

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,37 @@ import 'package:flutter/material.dart';
 void main() {
   // 플러터 앱을 실행한다.
   runApp(
-    // materialApp은 항상 최상위에 위치한다.
-    // scaffold는 바로 아래에 위치한다.
-    // Wedget
-    MaterialApp(
-      home: Scaffold(
+      // materialApp은 항상 최상위에 위치한다.
+      // scaffold는 바로 아래에 위치한다.
+      // Wedget
+      MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         backgroundColor: Color(0xFF335CB0),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('asset/image/logo.png'),
-            CircularProgressIndicator(
-              color: Colors.white,
-            )
-          ],
-        ),
-      ),
-    ),
-  );
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 100.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('asset/image/logo.png'),
+              SizedBox(height: 28.0),
+              CircularProgressIndicator(
+                color: Colors.white,
+              )
+            ],
+          ),
+        )
+    );
+  }
 }
